d469899828c02db6f786aa552e0487e7300ef5c1
Luis Ibanez <luis.ibanez@kitware.com>
2014-04-26 11:22:33 -0400

    Improving parsing of list.

--- MailingList/mailboxParsing.py
+++ MailingList/mailboxParsing.py
@@ -32,25 +32,26 @@ for message in mbox:
 
 for message in mbox:
   message_id = message['Message-Id']
-  message_from = message['From']
   message_reply = message['In-Reply-To']
-  message_date = message['Date']
+  message_from = message['From']
 
-  if message_id:
-    messages[message_id]={}
+  if message_reply:
+    if message_reply in messages:
+      previous_message = messages[message_reply]
 
-    if message_reply:
-      if message_reply in messages:
-        previous_message=messages[message_reply]
+      if previous_message:
+        recipient = previous_message['From'].itervalues().next()
 
-        if previous_message:
-          recipient=previous_message['From'].itervalues().next()
+        if recipient:
+          messages[message_id]['To']={}
+          messages[message_id]['To'][recipient]=''
 
-          if recipient:
-            messages[message_id]['To']={}
-            messages[message_id]['To'][recipient]=''
+          people[recipient]['Received']={}
+          people[recipient]['Received'][message_id]=''
 
-
+          sender = message_from
+          people[recipient]['ReceivedFrom'][message_from][message_id]=''
+          people[sender]['ReceivedFrom'][recipient][message_id]=''
 
 for person in people:
   print person,' ', len(person)
