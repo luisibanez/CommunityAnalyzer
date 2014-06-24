7b82f559c3969c5a7617e7eb0ab24a296d12b5ea
Jeffrey Baumes <jeff.baumes@kitware.com>
2014-06-24 14:55:28 -0400

    Adding sleep and checks for data already collected

--- BugTracker/bugzillaHistory.py
+++ BugTracker/bugzillaHistory.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import pymongo
 import json
+import time
 
 base_url = sys.argv[1]
 host = sys.argv[2]
@@ -13,9 +14,12 @@ in_coll = pymongo.MongoClient(host)[db][input_collection]
 out_coll = pymongo.MongoClient(host)[db][output_collection]
 
 url = base_url + '/rest/bug/'
-for d in in_coll.find(sort=[('creation_time', pymongo.DESCENDING)]):
+for d in in_coll.find(sort=[('creation_time', pymongo.DESCENDING)], timeout=False):
     print d['creation_time']
+    if out_coll.find_one({'id': d['id']}):
+        continue
     content = requests.get(url + str(d['id']) + '/history').content
     loaded = json.loads(content)
     d['history'] = loaded['bugs'][0]['history']
     out_coll.insert(d)
+    time.sleep(1)
